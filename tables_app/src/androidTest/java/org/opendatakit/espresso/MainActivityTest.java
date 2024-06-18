9db85b89a7bc42cc5c083446f6d3c4657e288281
Ihedioha Chinedu Innocent <ihediohachinedu21@gmail.com>
2024-06-18 21:17:55 +0100

    test toolbar visibility and menu items

--- tables_app/src/androidTest/java/org/opendatakit/espresso/MainActivityTest.java
+++ tables_app/src/androidTest/java/org/opendatakit/espresso/MainActivityTest.java
@@ -2,12 +2,17 @@ package org.opendatakit.espresso;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.intent.Intents.intended;
 import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 import androidx.test.core.app.ActivityScenario;
+import androidx.test.espresso.action.ViewActions;
 import androidx.test.espresso.intent.Intents;
+import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.rule.GrantPermissionRule;
 
@@ -62,5 +67,24 @@ public class MainActivityTest {
 
         // Check if the Export activity is launched
         intended(hasComponent(ExportCSVActivity.class.getName()));
+    }
+
+    @Test
+    public void testToolBarVisibility() {
+        onView(withId(R.id.toolbarMainActivity)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testMenuItems() {
+        // Open the overflow menu
+        onView(ViewMatchers.withContentDescription("More options"))
+                .perform(ViewActions.click());
+
+        // Check that specific menu items are displayed
+        onView(withText(R.string.about))
+                .check(matches(isDisplayed()));
+
+        onView(withText(R.string.preferences))
+                .check(matches(isDisplayed()));
     }
 }
