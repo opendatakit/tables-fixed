fe2f2ee452e2440eeb095fc9d942d449ebcad583
Ihedioha Chinedu Innocent <ihediohachinedu21@gmail.com>
2024-07-03 14:49:44 +0100

    add tables manager test

--- tables_app/src/androidTest/java/org/opendatakit/espresso/MainActivityTest.java
+++ tables_app/src/androidTest/java/org/opendatakit/espresso/MainActivityTest.java
@@ -4,10 +4,16 @@ import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasAction;
 import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasExtra;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import static org.hamcrest.Matchers.allOf;
+import android.content.ComponentName;
+import android.content.Intent;
 
 import androidx.test.core.app.ActivityScenario;
 import androidx.test.espresso.action.ViewActions;
@@ -20,6 +26,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import org.opendatakit.consts.IntentConsts;
 import org.opendatakit.tables.R;
 import org.opendatakit.tables.activities.ExportCSVActivity;
 import org.opendatakit.tables.activities.ImportCSVActivity;
@@ -86,5 +93,15 @@ public class MainActivityTest {
 
         onView(withText(R.string.preferences))
                 .check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testOpenTableManager() {
+        onView(withId(R.id.menu_table_manager_sync)).perform(click());
+
+        intended(allOf(hasComponent(new ComponentName(IntentConsts.Sync.APPLICATION_NAME,
+                        IntentConsts.Sync.ACTIVITY_NAME)),
+                hasAction(Intent.ACTION_DEFAULT),
+                hasExtra(IntentConsts.INTENT_KEY_APP_NAME, appName)));
     }
 }
